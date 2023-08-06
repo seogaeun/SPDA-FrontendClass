7d3f7aa1a4eaa70e5dd0676d4265124f9d80a51e
seogaeun <se0323333@gmail.com>
2023-08-07 08:09:05 +0900

    daay 07 update

---
+++ Day4/js/layout.js
@@ -0,0 +1,8 @@
+window.onload=function(){
+    let ele1 = document.getElementsByClassName("main_menu")[0];
+
+    ele1.addEventListener('click',function(){
+        document.getElementsByTagName('nav')[0].classList.toggle('visible');
+        console.log('a');
+    });
+}
