b98015ca18ae2ebe181449d790ac181bc7951a3b
seogaeun <se0323333@gmail.com>
2023-08-10 16:50:37 +0900

    d

--- FrontEnd/FEclass_0810/js/tap.js
+++ FrontEnd/FEclass_0810/js/tap.js
@@ -39,7 +39,7 @@ mytabs.forEach(item=>{
         //이벤트에 대한 정보를 받았으니
         //e는 a태그를 포커스해서 발생한 정보니깐, 아까 썼던 classlist를 add만해주기
         //나머지는 삭제해야하니깐 for문 돌려서 나머지 active된건 삭제
-        //visit와 visit패널
+        //visit와 visit패널b
         //이벤트 정보들은 add
         //나머지는 지워야함
         //add remove
